304279befb884c779acb41e3c6af3dd4c43d4c20
Denis Ivanchenko <sicknature24@yandex.com>
2024-01-10 15:22:34 +0300

    feat: add template for favorite component

--- src/app/favorite/pages/favorite/favorite.component.ts
+++ src/app/favorite/pages/favorite/favorite.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { IYoutubeItem } from '../../../youtube/models/youtube-search';
+import { AppState } from '../../../core/root.state';
+import { selectFilteredItems } from '../../state/favorite.selectors';
 
 @Component({
   selector: 'app-favorite',
   templateUrl: './favorite.component.html',
-  styleUrls: ['./favorite.component.scss']
+  styleUrls: ['./favorite.component.scss'],
 })
 export class FavoriteComponent {
+  @Input() searchItem?: IYoutubeItem;
 
+  filteredItems$: Observable<IYoutubeItem[]>;
+
+  constructor(private store: Store<AppState>) {
+    this.filteredItems$ = this.store.select(selectFilteredItems);
+  }
 }
--- src/app/favorite/state/favorite.selectors.ts
+++ src/app/favorite/state/favorite.selectors.ts
@@ -1,10 +1,19 @@
 import { createSelector } from '@ngrx/store';
 import { AppState } from '../../core/root.state';
 import { FavoriteState } from './favorite.reducer';
+import { selectAllItems } from '../../youtube/state/youtube.selectors';
 
 export const selectFavoriteFeature = (state: AppState): FavoriteState => state.favorite;
 
 export const selectFavoriteItems = createSelector(
   selectFavoriteFeature,
   (favoriteState: FavoriteState) => favoriteState.itemsIds
+);
+
+export const selectFilteredItems = createSelector(
+  selectAllItems,
+  selectFavoriteItems,
+  (items, ids) => {
+    return items.filter((item) => ids.includes(String(item.id)));
+  }
 );
